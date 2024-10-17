e299126a0963ac51407d4aba8ba8b8236a026f76
rnandory <tntjs2637@gmail.com>
2024-10-17 21:42:16 +0900

    h-pg-list카드목록이 1092px까지 늘어나도록 수정
    - 카드목록 section에 layout-list클래스 부여하고 flex-grow:1추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,6 +3,14 @@ insert into member
 values (1, 'minjiiiii', 'kimminji', 'minji0603', '1234',
         '2024-09-04 16:42:12.357000', 'qs1018@naver.com','1994-06-03');
 
+
+insert into category (name) values ('Activity');
+insert into category (name) values ('Culture');
+insert into category (name) values ('Food');
+insert into category (name) values ('Nature');
+insert into category (name) values ('Shopping');
+
+
 insert into program
     (title, detail,
      category1, category2, language, start_time, end_time,
@@ -111,8 +119,3 @@ insert into program
  status, price, group_size_max, group_size_min, rating, reg_member_id)
 values ('18let''s drop by the hyundai', 'let''s drop by the hyundai department store and han-river', 'activity', null, 'english', '11:00', '14:00', 'unpublished', 15000, 4, 2, 2.5, 1);
 
-insert into program
-(title, detail,
- category1, category2, language, start_time, end_time,
- status, price, group_size_max, group_size_min, rating, reg_member_id)
-values ('19let''s drop by the hyundai', 'let''s drop by the hyundai department store and han-river', 'activity', null, 'english', '11:00', '14:00', 'unpublished', 15000,
