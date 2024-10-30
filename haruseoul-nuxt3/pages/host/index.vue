8da8706d62295b236c0cebc0677f0af7d7f46e82
minji <qs1018@naver.com>
2024-10-30 17:54:50 +0900

    디렉토리 구조 변경

--- haruseoul-nuxt3/pages/host/index.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <Head>
-    <Title> Host index 페이지 </Title>
-  </Head>
-
-  Host index 페이지
-</template>
--- haruseoul-nuxt3/pages/index.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <Head>
-    <Title> HaruSeoul index 페이지 </Title>
-    <Link rel="stylesheet" href="/assets/css/admin/style.css"/>
-  </Head>
-
-  HaruSeoul index 페이지
-</template>
