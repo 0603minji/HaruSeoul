ae11aa692b92def86c5fa15a37b9331111721c66
jvn1o <junok0987@naver.com>
2024-10-31 00:05:29 +0900

    guest/reservation/create할때
    git commit -m guest/reservation/create할때

--- backend/src/main/java/com/m2j2/haruseoul/guest/reservation/dto/ReservationCreateDto.java
+++ backend/src/main/java/com/m2j2/haruseoul/guest/reservation/dto/ReservationCreateDto.java
@@ -1,12 +1,12 @@
 package com.m2j2.haruseoul.guest.reservation.dto;
 
-import com.m2j2.haruseoul.entity.PublishedProgram;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.Instant;
+import java.time.LocalDate;
+import java.util.Date;
 
 @Data
 @Builder
@@ -18,7 +18,7 @@ public class ReservationCreateDto {
 
     private Long regMemberId;
 
-    private Instant programDate;
+//    private LocalDate programDate;
 
     private Integer reservationGroupSize;
 
---
+++ backend/src/main/java/com/m2j2/haruseoul/guest/reservation/dto/ReservationCreateResponseDto.java
@@ -0,0 +1,15 @@
+package com.m2j2.haruseoul.guest.reservation.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReservationCreateResponseDto {
+    private Long publishedProgramId;
+    private ReservationCreateDto reservationCreateDto;
+}
