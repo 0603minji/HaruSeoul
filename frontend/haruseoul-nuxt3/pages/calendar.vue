d69493fc6f09ef2afac098b8106dfdcfa56bb92f
rnandory <tntjs2637@gmail.com>
2024-11-01 18:17:16 +0900

    components/filter/Calendar.vue v1

---
+++ frontend/haruseoul-nuxt3/pages/calendar.vue
@@ -0,0 +1,24 @@
+<script setup>
+</script>
+<template>
+  <main>
+    calendar
+    <div class="container">
+      <FilterCalendar/>
+    </div>
+    <br>
+    <br>
+  </main>
+</template>
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+
+  margin: 0 100px;
+
+  > section {
+    //flex-grow: 1;
+  }
+}
+</style>
