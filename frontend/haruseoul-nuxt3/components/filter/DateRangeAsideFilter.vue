186fee493875ed8df595846727610549a0ff8d08
rnandory <tntjs2637@gmail.com>
2024-11-13 11:20:49 +0900

    DateRangeAsideFilter ~ DateRangePicker fix infinite recursive call

--- frontend/haruseoul-nuxt3/components/filter/DateRangeAsideFilter.vue
+++ frontend/haruseoul-nuxt3/components/filter/DateRangeAsideFilter.vue
@@ -13,10 +13,11 @@ const props = defineProps({
 const emit = defineEmits(['closeModal']);
 
 // Selected dates 프로그램 검색기간 [시작일 , 끝] or [날짜]
-const selectedDates = ref([]);
-watchEffect(() => {
-  selectedDates.value = props.selectedDates;
-})
+const selectedDates = ref(props.selectedDates);
+watch(() => props.selectedDates, (newOne) => selectedDates.value = newOne);
+
+// 자식인 DateRangePicker의 resetSelectedDatesHandler를 호출시키기 위한 토글
+const resetToggle = ref(false);
 
 // Handle selection change
 const updateSelectedDates = (selectedOptions) => {
@@ -25,16 +26,28 @@ const updateSelectedDates = (selectedOptions) => {
   console.log('         ->  selectedDates: ', selectedDates.value);
 };
 
-const closeModal = () => {
-  emit('closeModal', selectedDates.value);
-}
+const closeModal = () => emit('closeModal', selectedDates.value);
+
+const resetDateRangePicker = () => resetToggle.value = !resetToggle.value;
 
 </script>
 
 <template>
   <aside class="popup">
+    <!-- selectedDates를 표시. 눌렀을 때 아래 form태그 visible   -->
+    <header class="title">
+      <h1 class="font-size:8">기간 선택</h1>
+      <button
+          @click.prevent="resetDateRangePicker"
+          class="margin-left:auto n-btn n-btn:hover n-btn-bd:none n-icon n-icon-size:2 n-icon:reset n-icon-color:sub-1 n-deco color:sub-1">
+        초기화
+      </button>
+    </header>
+
     <form @submit.prevent="closeModal" class="popup-body" action="">
-      <DateRangePicker :selectedDates="selectedDates" @selection-changed="updateSelectedDates"/>
+      <DateRangePicker :selectedDates="selectedDates"
+                       :reset-toggle="resetToggle"
+                       @selection-changed="updateSelectedDates"/>
 
       <div class="submit">
         <button class="n-btn n-btn:hover n-btn-bg-color:sub n-btn-size:1">확인</button>
