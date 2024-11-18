13ab6605915453bde692cf4a8633ce21a3a6a322
rnandory <tntjs2637@gmail.com>
2024-11-18 15:38:29 +0900

    host/reservation api 구현 중

---
+++ backend/src/main/java/com/m2j2/haruseoul/host/reservation/controller/ReservationController.java
@@ -0,0 +1,17 @@
+package com.m2j2.haruseoul.host.reservation.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("HostReservationController")
+@RequestMapping("host/reservations")
+public class ReservationController {
+
+//    ReservationService reservationService;
+//
+//    @PutMapping("{rid}")
+//    public ResponseEntity<Void> update(@PathVariable(name = "rid") Long reservationId) {
+//
+//        return ResponseEntity.ok(reservationService.update(reservationId));
+//    }
+}
