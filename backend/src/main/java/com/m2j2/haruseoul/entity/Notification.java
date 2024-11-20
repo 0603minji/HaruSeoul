45c95b9b757ebae03ff579969a1c5094cd600e30
ashfortune <yjb1521@naver.com>
2024-11-20 10:06:00 +0900

    등록페이지 지도생성

---
+++ backend/src/main/java/com/m2j2/haruseoul/entity/Notification.java
@@ -0,0 +1,46 @@
+package com.m2j2.haruseoul.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.Instant;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Entity
+@Table(name = "notification")
+public class Notification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id" , nullable = false)
+    private Long id;
+
+    @Column(name = "sender_id")
+    private Long senderId;
+
+    @Column(name = "receiver_id")
+    private Long receiverId;
+
+    @Column(name = "program_id")
+    private Long programId;
+
+    @Column(name = "type")
+    private String type;
+
+    @ColumnDefault("current_timestamp()")
+    @Column(name = "reg_date")
+    @CreationTimestamp
+    private Instant regDate;
+
+    @Column(name = "is_read")
+    private Boolean isRead;
+
+}
